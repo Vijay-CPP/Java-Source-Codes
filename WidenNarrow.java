59e8f0c29139aeff80e68d0c6d80923e59a6b529
Vijay <yourstrulyvijay12@gmail.com>
2022-08-15 23:10:46 +0530

    Updated

--- WidenNarrow.java
+++ WidenNarrow.java
@@ -5,12 +5,12 @@ class WidenNarrow // Upcast Downcast
     public static void main(String arg[])
     {
         int i = 10;
-        byte b = 20;
+        // byte b = 20;
         short s = 30;
         float f = 3.12f;
-        boolean bool = true;
-        double d = 123.123123;
-        long l = 123456789;
+        // boolean bool = true;
+        // double d = 123.123123;
+        // long l = 123456789;
 
         // i = f; not allowed Narrow
         i = (int)f;
